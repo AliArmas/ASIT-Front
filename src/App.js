07a978ffb43bb9c6055bb0ac208d123fe43a54ca
AliArmas <191287@ids.upchiapas.edu.mx>
2022-06-29 02:01:15 -0500

    add routes app-0201

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
+import ShowClients from './components/ShowClients'
+import EditCliente from './components/EditClient'
+import CreateClient from './components/CreateClient'
+
 function App() {
   return (
     <div className="App">
-     <button className='btn btn-primary'> Clear</button>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ShowClients/>} />
+          <Route path='/create' element={<CreateClient/>} />
+          <Route path='/edit/:id' element={<EditCliente/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
